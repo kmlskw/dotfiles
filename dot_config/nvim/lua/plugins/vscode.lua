923cea75bfc8acbaabc09422c98e7358e12627fa
Kamil Laskowski <kam.laskowski@gmail.com>
2024-03-12 17:03:41 +0100

    vscode-neovim keymap references-view

--- dot_config/nvim/lua/plugins/vscode.lua
+++ dot_config/nvim/lua/plugins/vscode.lua
@@ -1,5 +1,9 @@
+local map = vim.api.nvim_set_keymap
+
 if vim.g.vscode then
   -- VSCode extension
+  map("n", "gr", "<Cmd>call VSCodeNotify('references-view.findReferences')<CR>", { noremap = true, silent = false })
+  map("n", "gR", "<Cmd>call VSCodeNotify('references-view.tree.focus')<CR>", { noremap = true, silent = false })
 else
   return {}
 end
