48400c515b378da0686bb93f63f567850ee3b29d
Kamil Laskowski <kam.laskowski@gmail.com>
2024-03-17 11:32:01 +0100

    maps whichkey

--- dot_config/nvim/lua/plugins/vscode.lua
+++ dot_config/nvim/lua/plugins/vscode.lua
@@ -2,8 +2,13 @@ local map = vim.api.nvim_set_keymap
 
 if vim.g.vscode then
   -- VSCode extension
+  -- References
   map("n", "gr", "<Cmd>call VSCodeNotify('references-view.findReferences')<CR>", { noremap = true, silent = false })
   map("n", "gR", "<Cmd>call VSCodeNotify('references-view.tree.focus')<CR>", { noremap = true, silent = false })
+  -- Which Key by VSpaceCode
+
+  map("n", "<Space>", "<Cmd>call VSCodeNotify('whichkey.show')<CR>", { noremap = true, silent = true })
+  map("x", "<Space>", "<Cmd>call VSCodeNotify('whichkey.show')<CR>", { noremap = true, silent = true })
 else
   return {}
 end
